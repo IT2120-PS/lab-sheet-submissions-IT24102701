1452fefea48943d4913c3304bde652e81b51a0d4
IT24102701 <it24102701@my.sliit.lk>
2025-08-29 22:59:27 +0530

    Add IT24102701 to Lab Sheet 05

---
+++ Lab Sheet 05/IT24102701.R
@@ -0,0 +1,19 @@
+setwd("C:\\Users\\REDTECH\\Desktop\\IT24102701")
+
+Delivery_Times <- read.table("Exercise - Lab 05.txt", header = TRUE, sep = ",")
+fix(Delivery_Times)
+attach(Delivery_Times)
+
+#Histogram for Deliver Times
+hisogram <- hist(Delivery_Time_.minutes., main = "Histogram for Delivery Times", breaks = seq(20, 70, length = 5), right = TRUE)
+
+#Cumultative Frequency Polygon
+names(Delivery_Times)[1] <- "DeliveryTime"
+
+str(Delivery_Times)
+
+hist_data <- hist(Delivery_Times$DeliveryTime, breaks = seq(20, 70, length.out = 10), right = FALSE, plot = FALSE)
+cum_freq <- cumsum(hist_data$counts)
+breaks <- hist_data$breaks
+
+plot(breaks, c(0, cum_freq), type = "l", main = "Cumulative Frequency Polygon (Ogive)", xlab = "Delivery Time", ylab = "Cumulative Frequency", ylim = c(0, max(cum_freq)))
