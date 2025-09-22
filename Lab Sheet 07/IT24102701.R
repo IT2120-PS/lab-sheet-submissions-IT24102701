86e2463d88b86a1d8c62283df0a4dd3c3c15bb6d
IT24102701 <it24102701@my.sliit.lk>
2025-09-22 23:14:19 +0530

    Add IT24102701 to Lab Sheet 07

---
+++ Lab Sheet 07/IT24102701.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\akila\\Desktop\\IT24102701")
+
+#1
+punif(15, min = 0, max = 40, lower.tail = FALSE)
+
+#2
+pexp(2, rate = 0.333, lower.tail = TRUE)
+
+#3
+  #1.
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+  #2.
+qnorm(0.95, mean = 100, sd = 15)
